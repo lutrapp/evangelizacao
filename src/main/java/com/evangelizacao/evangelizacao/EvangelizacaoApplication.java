4ccc5782a0e9764b106cff02a99277750cd1c16f
lutrapp <lutrapp@gmail.com>
2025-03-04 20:14:02 -0300

    first commit

---
+++ src/main/java/com/evangelizacao/evangelizacao/EvangelizacaoApplication.java
@@ -0,0 +1,13 @@
+package com.evangelizacao.evangelizacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvangelizacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvangelizacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evangelizacao/evangelizacao/EvangelizacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evangelizacao.evangelizacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvangelizacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
